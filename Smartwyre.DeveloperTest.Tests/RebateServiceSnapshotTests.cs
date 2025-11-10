a4e10d4373b206c163b5577738a6f7404f3796f9
JJ <jj.orozcolopez.13@gmail.com>
2025-11-10 15:33:51 -0600

    feat: Setup characterization tests environment
    Adds necessary testing infrastructure to characterize the current behavior of RebateService before refactoring.
    
    - Added project reference to Smartwyre.DeveloperTest.
    - Installed required NuGet packages: xUnit, Moq, and Verify.Xunit (for characterization).
    - Removed the irrelevant and failing PaymentServiceTests class.
    - Created RebateServiceSnapshotTests class.

---
+++ Smartwyre.DeveloperTest.Tests/RebateServiceSnapshotTests.cs
@@ -0,0 +1,14 @@
+﻿using Smartwyre.DeveloperTest.Services;
+
+namespace Smartwyre.DeveloperTest.Tests
+{
+    public class RebateServiceSnapshotTests
+    {
+        private readonly RebateService _service;
+
+        public RebateServiceSnapshotTests()
+        {
+            
+        }
+    }
+}
