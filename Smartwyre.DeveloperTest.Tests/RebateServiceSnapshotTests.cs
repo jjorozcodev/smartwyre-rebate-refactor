2379c450fbcf18d3deae71b282978fc1cf253fd2
JJ <jj.orozcolopez.13@gmail.com>
2025-11-10 16:08:33 -0600

    test: Verify mocking harness and service invocation
    Adds a quick test to validate the integrity of the testing harness.

--- Smartwyre.DeveloperTest.Tests/RebateServiceSnapshotTests.cs
+++ Smartwyre.DeveloperTest.Tests/RebateServiceSnapshotTests.cs
@@ -1,14 +1,43 @@
-﻿using Smartwyre.DeveloperTest.Services;
+﻿using Moq;
+using Smartwyre.DeveloperTest.Data;
+using Smartwyre.DeveloperTest.Services;
+using Smartwyre.DeveloperTest.Types;
+using Xunit;
 
 namespace Smartwyre.DeveloperTest.Tests
 {
     public class RebateServiceSnapshotTests
     {
         private readonly RebateService _service;
+        private readonly Mock<IRebateDataStore> _rebateDataStoreMock;
+        private readonly Mock<IProductDataStore> _productDataStoreMock;
 
         public RebateServiceSnapshotTests()
         {
-            
+            _rebateDataStoreMock = new Mock<IRebateDataStore>();
+            _productDataStoreMock = new Mock<IProductDataStore>();
+
+            _service = new RebateService(_rebateDataStoreMock.Object, _productDataStoreMock.Object);
+        }
+
+        [Fact]
+        public void RebateService_Calculate_ShouldReturnSuccess_WhenIsFixedCashAmount()
+        {
+            CalculateRebateRequest request = new CalculateRebateRequest
+            {
+                ProductIdentifier = "PRD",
+                RebateIdentifier = "RBT"
+            };
+
+            Product product = new() { SupportedIncentives = SupportedIncentiveType.FixedCashAmount };
+            Rebate rebate = new() { Incentive = IncentiveType.FixedCashAmount, Amount = 100 };
+
+            _rebateDataStoreMock.Setup(x => x.GetRebate(It.IsAny<string>())).Returns(rebate);
+            _productDataStoreMock.Setup(x => x.GetProduct(It.IsAny<string>())).Returns(product);
+
+            var result = _service.Calculate(request);
+
+            Assert.True(result.Success);
         }
     }
 }
